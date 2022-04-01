29b63f09a7cbe0cedc40e4165715115432c9f0a7
plmokn210 <61922986+plmokn210@users.noreply.github.com>
2022-04-01 17:35:22 -0400

    smart contract fin

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,11 @@
-//https://eth-ropsten.alchemyapi.io/v2/HvOQEppk2xp-SCGmTB0hCHm6JrQOk5Xg
 require("dotenv").config();
-
-require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/HvOQEppk2xp-SCGmTB0hCHm6JrQOk5Xg",
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/HvOQEppk2xp-SCGmTB0hCHm6JrQOk5Xg",
       accounts: [process.env.PRIVATE_KEY],
     },
   },
