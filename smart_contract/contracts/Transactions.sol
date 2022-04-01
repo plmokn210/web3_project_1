0e987a17d46531d7f92550072a06ed6fda03bfb3
plmokn210 <61922986+plmokn210@users.noreply.github.com>
2022-04-01 14:40:33 -0400

    transactions contract fin

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -21,12 +21,14 @@ contract Transactions {
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
         transactionCount += 1;
         transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
     function getAllTransactions() public view returns(TransferStruct[] memory) {
-        //return transactions;
+        return transactions;
     }
 
     function getTransactionCount() public view returns(uint256) {
-        //return transactionCount;
+        return transactionCount;
     }
 }
