815f9a5fa0d5132880eec412cd10182cd8ca1ced
plmokn210 <61922986+plmokn210@users.noreply.github.com>
2022-04-05 16:52:10 -0400

    transaction div

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,11 @@
 import React, { useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
 import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
+
+const TransactionCard = ({ addressTo, addressFrom, timestamp, amount, url }) => {
+  return 'transaction'
+}
 
 const Transactions = () => {
   const { currentAccount } = useContext(TransactionContext);
@@ -16,6 +21,12 @@ const Transactions = () => {
             Connect your account to see the latest transactions
           </h3>
         )}
+
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {dummyData.reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
+          ))}
+        </div>
       </div>
     </div>
   );
